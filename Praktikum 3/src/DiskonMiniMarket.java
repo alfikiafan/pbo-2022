94bca01345f4caa6b4dd1b6a28e8e65128281175
Alfiki Diastama Afan Firdaus <89908618+alfikiafan@users.noreply.github.com>
2022-09-08 13:10:06 +0700

    Praktikum 3

---
+++ Praktikum 3/src/DiskonMiniMarket.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DiskonMiniMarket {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Total pembelian: ");
+        // tambahkan code untuk menyimpan nominal total pembelian
+        double total_pembelian = sc.nextInt();
+        // tambahkan code untuk mengimplementasikan ketentuan diskon yang ditetapkan
+        if (total_pembelian >= 50000 && total_pembelian <= 75000)
+            total_pembelian = 0.95 * total_pembelian;
+        else if (total_pembelian >= 75000 && total_pembelian <= 125000)
+            total_pembelian = 0.85 * total_pembelian;
+        else total_pembelian = 0.8 * total_pembelian;
+        // tambahkan code untuk menampilkan nominal yang harus dibayar sesuai dengan ketentuan diskon yang diberikan
+        System.out.println("Total pembelian setelah diskon: " + total_pembelian);
+    }
+}
